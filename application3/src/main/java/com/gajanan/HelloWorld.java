2b8165fa8edfde33844233a6a42173b1bc0af745
gajanan <gajanan@gajanan>
2017-07-05 01:14:07 +0530

    first commit

---
+++ application3/src/main/java/com/gajanan/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.gajanan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld {
+
+	@RequestMapping("/hello")  
+    public ModelAndView helloWorld() {  
+        String message = "HELLO SPRING MVC HOW R  U";  
+        return new ModelAndView("home", "message", message);  
+    }  
+}
