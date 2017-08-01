edc3722db1292d4dd2afd819967e5a66aef6e416
Harsh <Harsh@192.168.0.6>
2017-08-01 21:02:04 +0530

    Messages.properties added

---
+++ application3/src/main/java/com/application/controller/LoginController.java
@@ -0,0 +1,74 @@
+package com.application.controller;
+
+
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.application.core.User;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	User usr;
+	
+	@RequestMapping("/login") 
+	 public ModelAndView doLogin(HttpServletRequest request, HttpServletResponse response)
+	{
+		 String name=request.getParameter("name");  
+	     String password=request.getParameter("password");
+	    
+	     usr.setUserID(name);
+	     usr.setPassword(password);
+	     
+	     ModelAndView modelAndView=new ModelAndView();
+	     modelAndView.addObject("user", usr);
+	     
+	     if(password.equals("admin")){  
+	         String message = "HELLO "+name;  
+	         return  new ModelAndView("hellopage", "message", message);  
+	       }  
+	     else{  
+	             return new ModelAndView("errorpage", "message","Sorry, username or password error");  
+	         }  
+		
+	 	
+	}
+	
+	@RequestMapping(value="/register",method = RequestMethod.GET)
+	public String dologin1(HttpServletRequest request, HttpServletResponse response,Model model)
+	{
+		String name=request.getParameter("name");  
+	     String password=request.getParameter("password");
+	    
+	     usr.setUserID(name);
+	     usr.setPassword(password);
+	     
+	  
+	     model.addAttribute("user", usr);
+	     System.out.println(name + " "+password);
+	     if(password.equals("admin")){  
+	         String message = "HELLO "+name;  
+	      
+	         model.addAttribute("message", message);
+	         return  "hellopage";  
+	       }  
+	     else{  
+	    	
+	    	 model.addAttribute("message", "Sorry username and password is wrong !" );
+	             return "errorpage";  
+	         }  
+		
+		
+	
+	}
+	
+	
+
+}
--- application3/src/main/webapp/WEB-INF/jsp/hellopage.jsp
+++ application3/src/main/webapp/WEB-INF/jsp/hellopage.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +10,6 @@
 <title>hello</title>
 </head>
 <body>
-Message is: ${message} 
+    Message is: ${message}   
 </body>
 </html>
