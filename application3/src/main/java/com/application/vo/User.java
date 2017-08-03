a7c0554575e7c78cd2083cd68f9c81a34fbc7efd
gajanan <gajanan@192.168.0.5>
2017-08-03 08:20:04 +0530

    two more packages added

---
+++ application3/src/main/java/com/application/vo/User.java
@@ -0,0 +1,20 @@
+package com.application.vo;
+
+public class User {
+	
+	private String userID;
+	private String password;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
